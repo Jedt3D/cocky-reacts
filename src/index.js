a32a1aaf97aec22c44ef052e631a66d92eb9d428
Worajedt Sitthidumrong <sjedt@3ddaily.com>
2021-07-25 03:20:28 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>อาหารที่ฉันชอบ</h1>
+    <ul>
+      <li>มะม่วง</li>
+      <li>ทุเรียน</li>
+      <li>เงาะ</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
